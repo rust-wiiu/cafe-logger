b40e496a669441707208d409ed7fa708adb71362
29th-Day <43274844+29th-Day@users.noreply.github.com>
2026-02-20 02:21:51 +0100

    Add Cargo.lock and UDP logging support

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,17 @@
 use cafe_rs::prelude::*;
 use log::{Level, Metadata, Record, SetLoggerError};
 
-use std::{cell::Cell, ffi::CString};
+use cafe::net::socket::Socket;
+use std::{
+    cell::{Cell, RefCell},
+    ffi::CString,
+    net::{SocketAddrV4, ToSocketAddrs},
+};
 
 pub struct CafeLogger {
     level: Cell<Level>,
     console: Cell<bool>,
+    udp: RefCell<Option<(Socket, SocketAddrV4)>>,
 }
 
 unsafe impl Sync for CafeLogger {}
@@ -19,25 +25,40 @@ impl CafeLogger {
         &LOGGER
     }
 
+    #[inline]
     pub const fn default() -> Self {
         Self {
             level: Cell::new(Level::Warn),
             console: Cell::new(true),
+            udp: RefCell::new(None),
         }
     }
 
     /// Log level
+    #[inline]
     pub fn level(self, level: Level) -> Self {
         Self::get().level.replace(level);
         self
     }
 
     /// Internal debug console
+    #[inline]
     pub fn console(self, enabled: bool) -> Self {
         Self::get().console.replace(enabled);
         self
     }
 
+    /// Network UDP port
+    #[inline]
+    pub fn udp(self, address: impl ToSocketAddrs) -> Self {
+        Self::get().udp.replace(Some((
+            Socket::udp().unwrap(),
+            address.to_socket_addrs().unwrap().next().unwrap(),
+        )));
+        self
+    }
+
+    #[inline]
     pub fn init(self) -> Result<(), SetLoggerError> {
         log::set_max_level(Self::get().level.get().to_level_filter());
         log::set_logger(Self::get())
@@ -45,6 +66,7 @@ impl CafeLogger {
 }
 
 impl log::Log for CafeLogger {
+    #[inline]
     fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= self.level.get()
     }
@@ -57,6 +79,9 @@ impl log::Log for CafeLogger {
                     sys::coreinit::debug::report(str.as_ptr());
                 }
             }
+            if let Some((socket, addr)) = self.udp.borrow().as_ref() {
+                socket.send_to(str.as_bytes(), addr, None);
+            }
         }
     }
 
@@ -66,4 +91,18 @@ impl log::Log for CafeLogger {
 /// Convenience function for initializing the logger with default settings.
 pub fn init() -> Result<(), SetLoggerError> {
     CafeLogger::default().init()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        CafeLogger::default()
+            .level(log::Level::Debug)
+            .console(true)
+            .init()
+            .unwrap();
+    }
 }
