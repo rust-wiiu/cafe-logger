08766259d5794ae3ccbc29f68900d24a631925d5
29th-Day <43274844+29th-Day@users.noreply.github.com>
2026-03-03 16:58:36 +0100

    UdpSocket, heapless, documenation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
+//! A logger that prints [log][https://crates.io/crates/log] messages to configured output channels on the Wii U. It is the Rust alternative to the [WHBLog][https://github.com/devkitPro/wut/blob/master/libraries/libwhb/include/whb/log.h] functions in [wut][https://github.com/devkitPro/wut].
+//!
+//! # Example
+//!
+//! ```
+//! use cafe_logger::CafeLogger;
+//! fn main() {
+//!     CafeLogger::new().init().unwrap();
+//!
+//!     log::warn!("This is an example message.");
+//! }
+//! ```
+
 #![no_std]
 
 use cafe_rs::prelude::*;
-use log::{Level, Metadata, Record, SetLoggerError};
 
+use log::{Level, Metadata, Record, SetLoggerError};
 use std::{
     cell::{Cell, RefCell},
-    ffi::CString,
-    net::{Ipv4Addr, ToSocketAddrs, UdpSocket},
+    net::{ToSocketAddrs, UdpSocket},
 };
 
 pub struct CafeLogger {
@@ -15,17 +27,26 @@ pub struct CafeLogger {
     udp: RefCell<Option<UdpSocket>>,
 }
 
+// This is only correct if the logger is once initialized from the main thread and never changed after that. Messages from threads may intertwine.
 unsafe impl Sync for CafeLogger {}
 
 impl CafeLogger {
     #[inline]
     const fn get() -> &'static Self {
-        static LOGGER: CafeLogger = CafeLogger::default();
+        static LOGGER: CafeLogger = CafeLogger::new();
         &LOGGER
     }
 
+    /// Initializes the global logger with default log level set to [Level::Warn] and [console] enabled.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use cafe_logger::CafeLogger;
+    /// CafeLogger::new().init().unwrap();
+    /// ```
     #[inline]
-    pub const fn default() -> Self {
+    pub const fn new() -> Self {
         Self {
             level: Cell::new(Level::Warn),
             console: Cell::new(true),
@@ -33,31 +54,54 @@ impl CafeLogger {
         }
     }
 
-    /// Log level
+    /// Sets the log level of the logger instance.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use cafe_logger::CafeLogger;
+    /// use log::Level;
+    /// CafeLogger::new().level(Level::Info).init().unwrap();
+    /// ```
     #[inline]
     pub fn level(self, level: Level) -> Self {
         Self::get().level.replace(level);
         self
     }
 
-    /// Internal debug console
+    /// Enables or disables logging to the internal console.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use cafe_logger::CafeLogger;
+    /// CafeLogger::new().console(false).init().unwrap();
+    /// ```
     #[inline]
     pub fn console(self, enabled: bool) -> Self {
         Self::get().console.replace(enabled);
         self
     }
 
-    /// Network UDP port
+    /// Enables or disabled logging to a (remote) UDP port.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use cafe_logger::CafeLogger;
+    /// CafeLogger::new().udp("1.2.3.4:4405").init().unwrap();
+    /// ```
     #[inline]
     pub fn udp(self, address: impl ToSocketAddrs) -> Self {
         Self::get().udp.replace(Some({
-            let udp = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).unwrap();
+            let udp = UdpSocket::bind("0.0.0.0:0").unwrap();
             udp.connect(address).unwrap();
             udp
         }));
         self
     }
 
+    /// Initializes the global logger with the current settings of the logger instance. This method MUST be called ONCE in order for the logger to work.
     #[inline]
     pub fn init(self) -> Result<(), SetLoggerError> {
         log::set_max_level(Self::get().level.get().to_level_filter());
@@ -73,10 +117,14 @@ impl log::Log for CafeLogger {
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let str = CString::new(format!("{} - {}\n", record.level(), record.args())).unwrap();
+            // Use heapless instead of CString to avoid allocations, which in turn enables the use of log regardless of allocator setup
+            let mut str = heapless::String::<512>::new();
+            use core::{fmt::Write, write};
+            let _ = write!(str, "{} - {:.500}\n\0", record.level(), record.args());
+
             if self.console.get() {
                 unsafe {
-                    sys::coreinit::debug::report(str.as_ptr());
+                    sys::coreinit::debug::report(str.as_ptr().cast());
                 }
             }
             if let Some(udp) = self.udp.borrow().as_ref() {
@@ -89,8 +137,10 @@ impl log::Log for CafeLogger {
 }
 
 /// Convenience function for initializing the logger with default settings.
+///
+/// Identical to calling `CafeLogger::new().init()`.
 pub fn init() -> Result<(), SetLoggerError> {
-    CafeLogger::default().init()
+    CafeLogger::new().init()
 }
 
 #[cfg(test)]
@@ -99,7 +149,7 @@ mod test {
 
     #[test]
     fn test() {
-        CafeLogger::default()
+        CafeLogger::new()
             .level(log::Level::Debug)
             .console(true)
             .init()
